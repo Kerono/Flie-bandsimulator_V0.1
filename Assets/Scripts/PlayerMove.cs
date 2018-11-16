260e70fa3af6e91eb675675de6fbfa2e75a3b4d7
Kerono <44676282+Kerono@users.noreply.github.com>
2018-11-16 10:05:42 +0100

    FPS Steuerung
    Character Controller für FPS steuerung wurde hinzugefügt

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField] private string horizontalInputName, verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>(); 
+    }
+    private void Update()
+    {
+        PlayerMovement();
+    }
+    private void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis(horizontalInputName) * movementSpeed * Time.deltaTime;
+        float verticalInput = Input.GetAxis(verticalInputName) * movementSpeed * Time.deltaTime;
+
+        Vector3 forwardMovement = transform.forward * verticalInput;
+        Vector3 sideMovement = transform.right * horizontalInput;
+
+        characterController.SimpleMove(forwardMovement + sideMovement);
+    }
+
+}
